83ff4eae88d53cae43b4e386e4ca23203d46f0c7
nevena29 <nevena.29@gmail.com>
2018-12-25 23:31:53 +0100

    singleAuthorPage

--- singlePost.js
+++ singlePost.js
@@ -1,9 +1,51 @@
-const userIdPost = localStorage.getItem("myPost");
-console.log(userIdPost);
-const url1 ="https://jsonplaceholder.typicode.com/posts/" + userIdPost;
+const idPost = localStorage.getItem("myPost");
+
+const container=$(".flex-container");
+const url1 ="https://jsonplaceholder.typicode.com/posts/" + idPost;
 
 fetch(url1)
 .then((resp)=>resp.json())
 .then(function(data){
     console.log(data)
-});
+ 
+    const title = data.title;
+    const body = data.body;
+    let   singleId = data.id;
+ container.append(
+         `<div class="secondPage"> 
+             <h2>${title} </h2>
+            <p> ${body} </p>
+         
+         </div>`
+     )
+  
+fetchSameAuthorPosts(singleId);
+    
+
+ });
+const url2 = "https://jsonplaceholder.typicode.com/posts?userId=" + idPost
+  function fetchSameAuthorPosts(singleId){
+    fetch(url2)
+    .then((resp)=>resp.json())
+    .then(function(authorsData){
+        for(i=0; i<authorsData.length;i++){
+            const title = authorsData[i].title
+            const body = authorsData[i].body;
+            const listUsersId = authorsData[i].id
+            if(singleId !== listUsersId){
+            container.append(
+                `<div> 
+                   <h2><a href="#">${title}</a> </h2>
+                </div>`
+            )
+        } 
+        $(document).on("click","a",function(singleId){
+          if(singleId!==listUsersId){
+
+          }
+        })
+        }
+
+
+  })
+}
